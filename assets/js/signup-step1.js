98ff9c3ea4c6bf159a2ef87f0860ba7385b94e31
Vaibhav-BRS <krishvaibu@gmail.com>
2024-04-04 11:36:14 +0530

    update

--- assets/js/signup-step1.js
+++ assets/js/signup-step1.js
@@ -292,22 +292,12 @@ function toggle() {
     return true
   }
 }
-
-function validationPhone() {
-  var phoneNumberInput = document.getElementById("phone_number");
-  var phoneNumber = phoneNumberInput.value;
-  // Regular expression for a valid phone number (simple example for illustration)
-  var phoneRegex = /^\+91 \d{10}$/;
-  if (!phoneRegex.test(phoneNumber)) {
-    //alert("Invalid phone number. Please enter a valid phone number.");
-    phoneNumberInput.style.borderColor = "red";
-    phoneNumberInput.classList.add("error");
-    return false;
-  } else {
-    // Remove error message and reset border
-    phoneNumberInput.classList.remove("error");
-    return true;
-  }
+function validatePhoneNumber(input) {
+  var phoneNumber = input.value;
+  // Remove any non-numeric characters
+  phoneNumber = phoneNumber.replace(/\D/g, '');
+  // Update the input value
+  input.value = phoneNumber;
 }
 function validateForm() {
   var countrySelect = document.getElementById('country');
